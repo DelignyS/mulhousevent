c6293b55481c4bdbd251e4da5a3f6dd00fd94752
JerkStanton <sylvain.deligny@yahoo.fr>
2023-11-08 10:21:08 +0100

    commit again

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,14 @@
-# Load the Rails application.
+
 require_relative "application"
 
-# Initialize the Rails application.
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['MAILJET_LOGIN'],
+  password: ENV['MAILJET_PWD'],
+  domain: 'https://mulhousevent-0b4c949ec7b1.herokuapp.com/',
+  address: 'in-v3.mailjet.com',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
 Rails.application.initialize!
